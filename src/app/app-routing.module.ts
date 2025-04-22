41cee76d447a67520dc84fbf079787388e29e720
Yaroslav Kostiv <slovjans89@yahoo.com>
2025-04-22 13:44:54 +0300

    List component was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   { path: 'main', component: ListComponent },
+  { path: 'user', component: UserComponent },
   { path: '', redirectTo: 'main', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
